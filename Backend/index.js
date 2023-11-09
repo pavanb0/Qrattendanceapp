f7c3f241ca53e6c76f5f2f7af2087d82dcf5a0ba
pavanb0 <pavanbagwe16@gmail.com>
2023-11-09 23:18:08 +0530

    Backend is ready

--- Backend/index.js
+++ Backend/index.js
@@ -5,8 +5,9 @@ const bcrypt = require('bcrypt');
 const app = express();
 const cors = require("cors");
 const port = 3000;
+const ip = "192.168.0.104";
 class qr {
-  constructor(qrcode,username){
+  constructor(qrcode, username) {
     this.qrcode = qrcode;
     this.username = username;
   }
@@ -14,6 +15,7 @@ class qr {
 app.use(bodyParser.json());
 app.use(cors());
 const db = new sqlite3.Database('database.db');
+
 
 // Create a users table
 db.serialize(() => {
@@ -27,8 +29,36 @@ db.serialize(() => {
   `);
 });
 
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE IF NOT EXISTS attendance (
+      id INTEGER PRIMARY KEY,
+      username TEXT,
+      date TEXT,
+      time TEXT,
+      qrcode INTEGER,
+      subject TEXT
+    )
+  `);
+});
+
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE IF NOT EXISTS subject (
+      id INTEGER PRIMARY KEY,
+      subject TEXT,
+      teacher TEXT,
+      qrcode INTEGER
+    )
+  `);
+});
+
+
+
+
+
 app.post('/signup', async (req, res) => {
-  const { username, password,teacher } = req.body;
+  const { username, password, teacher } = req.body;
 
   if (!username || !password) {
     return res.status(400).json({ error: 'Username and password are required' });
@@ -36,13 +66,13 @@ app.post('/signup', async (req, res) => {
 
   if (password.length < 8) {
     return res.status(400).json({ error: 'Password must be at least 8 characters' });
-}
+  }
   const hashedPassword = await bcrypt.hash(password, 10);
-  
-  if (teacher == 1){
+
+  if (teacher == 1) {
     db.run(
       'INSERT INTO users (username, password,teacher) VALUES (?, ?, ?)',
-      [username, hashedPassword,teacher],
+      [username, hashedPassword, teacher],
       (err) => {
         if (err) {
           return res.status(500).json({ error: 'User registration failed' });
@@ -50,18 +80,18 @@ app.post('/signup', async (req, res) => {
         res.status(201).json({ message: 'User registered successfully' });
       }
     );
-  }else{
-  db.run(
-    'INSERT INTO users (username, password) VALUES (?, ?)',
-    [username, hashedPassword],
-    (err) => {
-      if (err) {
-        return res.status(500).json({ error: 'User registration failed' });
+  } else {
+    db.run(
+      'INSERT INTO users (username, password) VALUES (?, ?)',
+      [username, hashedPassword],
+      (err) => {
+        if (err) {
+          return res.status(500).json({ error: 'User registration failed' });
+        }
+        res.status(201).json({ message: 'User registered successfully' });
       }
-      res.status(201).json({ message: 'User registered successfully' });
-    }
-  );
-}
+    );
+  }
 });
 
 app.post('/login', async (req, res) => {
@@ -95,13 +125,13 @@ app.post('/login', async (req, res) => {
 });
 
 
-app.post("/getqrcode",(req,res) => {
-    const { username, password,teacher } = req.body;
+app.post("/setqrcode", (req, res) => {
+  const { username, password, teacher, subject } = req.body;
 
-    if (!username || !password) {
-      return res.status(400).json({ error: 'Username and password are required' });
-    }
-    if (teacher == 1){
+  if (!username || !password) {
+    return res.status(400).json({ error: 'Username and password are required' });
+  }
+  if (teacher == 1) {
     db.get(
       'SELECT id, username, password FROM users WHERE username = ? and teacher = 1',
       [username],
@@ -109,32 +139,174 @@ app.post("/getqrcode",(req,res) => {
         if (err) {
           return res.status(500).json({ error: 'Internal server error' });
         }
-  
+
         if (!row) {
           return res.status(401).json({ error: 'User not found' });
         }
-  
+
         const passwordMatch = await bcrypt.compare(password, row.password);
-  
+
         if (!passwordMatch) {
           return res.status(401).json({ error: 'Incorrect password' });
         }
         const qrcode = Math.floor(Math.random() * 1000000);
         // hold the qrcode in ram and destroy after 5 minutes
-        qr = new qr(qrcode,username);
-        setTimeout(function(){
-          qr = null;
-        },300000);
-        qr.qrcode = qrcode;
-        res.json({s:s,message: 'Login successful', user: { id: row.id, username: row.username } });
+        // add the qrcode to the database
+        db.run(
+          'INSERT INTO subject (subject, teacher, qrcode) VALUES (?, ?, ?)',
+          [subject, username, qrcode],
+          (err) => {
+            if (err) {
+              return res.status(500).json({ error: 'User registration failed' });
+            }
+          
+          }
+        );
+
+        res.json({ message: 'Qr setted', user: { id: row.id, username: row.username } });
       }
     );
-    }else{
-        return res.status(401).json({ error: 'User not found' });
-    }
+  } else {
+    return res.status(401).json({ error: 'User not found' });
+  }
+
 
 })
 
-app.listen(port,"192.168.0.104", () => {
-  console.log(`Server is running on http://${"192.168.0.104"}:${port}`);
+app.post("/attendances", (req, res) => {
+  const {username, password,subject,qr} = req.body;
+  if (!username || !password) {
+    return res.status(400).json({ error: 'Username and password are required' });
+  }
+  db.get(
+    'SELECT id, username, password FROM users WHERE username = ?',
+    [username],
+    async (err, row) => {
+      if (err) {
+        return res.status(500).json({ error: 'Internal server error' });
+      }
+
+      if (!row) {
+        return res.status(401).json({ error: 'User not found' });
+      }
+
+      const passwordMatch = await bcrypt.compare(password, row.password);
+
+      if (!passwordMatch) {
+        return res.status(401).json({ error: 'Incorrect password' });
+      }
+      db.get(
+        "SELECT qrcode FROM subject WHERE subject = ?",[subject],
+          async (err, row) => {
+            if (row.qrcode == qr) {
+              db.run(
+                'INSERT INTO attendance (username, date, time, qrcode, subject) VALUES (?, ?, ?, ?, ?)',
+                [username, new Date().toLocaleDateString(), new Date().toLocaleTimeString(), qr, subject],
+                (err) => {
+                  if (err) {
+                    return res.status(500).json({ error: 'User registration failed' });
+                  }
+                  res.status(201).json({ message: 'attendace marked', user:  row.username  ,subject:subject});
+                }
+              );
+            }
+          
+          }
+        )
+      
+    }
+  );
+
+
+});
+
+
+app.post("/getqrcode", async(req, res) => {
+  const { username, password, subject } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ error: 'Username and password are required' });
+  } else {
+
+    db.get(
+      'SELECT id, username, password FROM users WHERE username = ? ',
+      [username],
+      async (err, row) => {
+        if (err) {
+          return res.status(500).json({ error: 'Internal server error' });
+        }
+
+        if (!row) {
+          return res.status(401).json({ error: 'User not found' });
+        }
+
+        const passwordMatch = await bcrypt.compare(password, row.password);
+
+        if (!passwordMatch) {
+          return res.status(401).json({ error: 'Incorrect password' });
+        }
+        db.get(
+          'SELECT qrcode FROM subject WHERE subject = ?',
+          [subject],
+          async (err, row) => {
+            if (err) {
+              return res.status(500).json({ error: 'Internal server error' });
+            }
+
+            if (!row) {
+              return res.status(401).json({ error: 'Internal server error' });
+            }
+            res.status(200).json({ qr: row.qrcode, message: "Succesful" })
+          }
+        );
+
+      }
+    );
+  }
+})
+
+
+app.post('/removeqr', (req, res) => {
+  const { username, password, teacher, subject } = req.body;
+  if (!username || !password) {
+    return res.status(400).json({ error: 'Username and password are required' });
+  }
+  if (teacher == 1) {
+    db.get(
+      'SELECT id, username, password FROM users WHERE username = ?',
+      [username],
+      async (err, row) => {
+        if (err) {
+          return res.status(500).json({ error: 'Internal server error' });
+        }
+
+        if (!row) {
+          return res.status(401).json({ error: 'User not found' });
+        }
+
+        const passwordMatch = await bcrypt.compare(password, row.password);
+
+        if (!passwordMatch) {
+          return res.status(401).json({ error: 'Incorrect password' });
+        }
+        db.run(
+          'DELETE FROM subject WHERE subject = ?',
+          [subject],
+          (err) => {
+            if (err) {
+              return res.status(500).json({ error: 'User registration failed' });
+            }
+            res.status(201).json({ message: 'User registered successfully' });
+          }
+        );
+        res.status(200).json({ message: 'qr session destroyed' });
+      }
+    );
+  } else {
+    return res.status(401).json({ error: 'User not found' });
+  }
+})
+
+app.listen(port, ip , () => {
+  console.log(`Server is running on http://${ip}:${port}`);
 });
--- Backend/test.py
+++ Backend/test.py
@@ -1,5 +1,5 @@
 import requests
-
+import time
 # Set the base URL of your API
 base_url = 'http://192.168.0.104:3000'
 
@@ -12,19 +12,94 @@ def signup(username, password):
 
 # Function to log in with a user account
 def login(username, password):
-    url = f'{base_url}/getqrcode'
+    url = f'{base_url}/login'
     data = {'username': username, 'password': password, 'teacher':"1"}
     response = requests.post(url, json=data)
     try:
         return response.json()
     except Exception as e:
         return {'error': 'Invalid response from the server'}
+    
+
+# app.post("/setqrcode",(req,res) => {
+#     const { username, password,teacher,subject} = req.body;
+def setqrcode(username,password,teacher,subject):
+    url = f'{base_url}/setqrcode'
+    data = {'username': username, 'password': password, 'teacher':teacher,'subject':subject}
+    response = requests.post(url, json=data)
+    try:
+        return response.json()
+    except Exception as e:
+        return {'error': 'Invalid response from the server'}
+
+
+# app.post('/removeqr',(req,res) => {
+#   const { username, password,teacher,subject} = req.body;
+def removeqr(username,password,teacher,subject):
+    url = f'{base_url}/removeqr'
+    data = {'username': username, 'password': password, 'teacher':teacher,'subject':subject}
+    response = requests.post(url, json=data)
+    try:
+        return response.json()
+    except Exception as e:
+        return {'error': 'Invalid response from the server'}
+    
+# app.post("/getqrcode",(req,res) => {
+#   const { username, password,subject} = req.body;
+
+def getqrcode(username,password,subject):
+    url = f'{base_url}/getqrcode'
+    data = {'username': username, 'password': password,'subject':subject}
+    response = requests.post(url, json=data)
+    try:
+        return response.json()
+    except Exception as e:
+        return {'error': 'Invalid response from the server'}
+
+
+# app.post("/attendances", (req, res) => {
+#   const {username, password,subject,qr} = req.body;
+
+def attendace(username,password,subject,qr):
+    url = f'{base_url}/attendances'
+    data = {'username': username, 'password': password,'subject':subject,'qr':qr}
+    response = requests.post(url, json=data)
+    try:
+        return response.json()
+    except Exception as e:
+        return {'error': 'Invalid response from the server'}
+
+
 # Example usage
 if __name__ == '__main__':
     # Test signup
-    # signup_response = signup('rhucha', 'goosbumps')
+    # time.sleep(1)
+    # signup_response = signup('pavan', 'pavan123')
     # print('Signup Response:', signup_response)
+    # time.sleep(1)
+    # # Test login
+    # login_response = login('pavan', 'pavan123')
+    # print('Login Response:', login_response)
+    # time.sleep(1)
+    # # Test setqrcode
 
-    # Test login
-    login_response = login('rhucha', 'goosbumps')
-    print('Login Response:', login_response)
+    # setqrcode_response = setqrcode('pavan', 'pavan123','1','Maths')
+    # print('setqrcode Response:', setqrcode_response)
+    # time.sleep(1)
+    # Test getqrcode
+    # getqrcode_response = getqrcode('pavan', 'pavan123','CHEMISTRY')
+    # print('getqrcode Response:', getqrcode_response)
+
+    # test attendance
+    #   app.post("/attendances", (req, res) => {
+    #   const {username, password,subject,qr} = req.body;
+    attendace_response = attendace('pavan', 'pavan123','CHEMISTRY','69422')
+    print('attendace Response:', attendace_response)
+
+
+
+    # # Test removeqr
+    # removeqr_response = removeqr('rhucha', 'goosbumps','1','1')
+    # print('removeqr Response:', removeqr_response)
+
+
